75b1e7a43865659958020f43def866430829755d
xwccc <53031813+xwccc@users.noreply.github.com>
2022-08-01 20:13:10 +0800

    Update chmod.sh

--- chmod.sh
+++ chmod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod +x ASC2SAC checkmode eigenfun.gmt eigenfun.gmt5 mineos mineos_drv mineos_merge mineos_mpi mineos_strip modesum7_mpi modesum7_nonmpi read_mineos
-chmod +x run.mineos.drv run.mineos.drv.mpi run.modesum run.mineos.drv.sustc
+chmod +x run.mineos.drv run.mineos.drv.mpi run.modesum
